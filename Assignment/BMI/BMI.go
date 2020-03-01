8d87ade871d5a5b09b9da7781d39428513340842
kjx123121 <59337045+kjx123121@users.noreply.github.com>
2020-03-01 21:17:04 +0800

    Add files via upload

--- Assignment/BMI/BMI.go
+++ Assignment/BMI/BMI.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Assignment/BMI/calculation"
+	"Assignment/BMI/getInput"
 	"fmt"
 	"math"
 )
@@ -22,10 +23,10 @@ var adultEnergy map[int]map[string][2]float64 = map[int]map[string][2]float64{
 func main() {
 
 
-	height := getHeight()
-	weight := getWeight()
-	exercise := getExercise()
-	diet := getDiet()
+	height := getInput.GetHeight()
+	weight := getInput.GetWeight()
+	exercise := getInput.GetExercise()
+	diet := getInput.GetDiet()
 
 
 
@@ -41,93 +42,4 @@ func main() {
 	minProtein, maxProtein := calculation.CalculateProtein(diet, weight)
 	fmt.Printf("每日摄取蛋白质范围为: %0.2f--%0.2f(g)\n", minProtein, maxProtein)
 
-}
-
-func stdinInt() int {
-	var intReceiver int
-	_, err := fmt.Scanf("%d", &intReceiver)
-
-	if err != nil {
-		fmt.Println(err)
-
-	}
-	return intReceiver
-}
-
-func stdinFloat() float64 {
-	var floatReceiver float64
-
-	_, err := fmt.Scanf("%f", &floatReceiver)
-
-	if err != nil {
-		fmt.Println(err)
-
-	}
-	return floatReceiver
-}
-
-func stdinBoolean() bool {
-	var booleanReceiver bool
-
-	_, err := fmt.Scanf("%t", &booleanReceiver)
-
-	if err != nil {
-		fmt.Println(err)
-
-	}
-	return booleanReceiver
-}
-
-
-func getHeight() float64{
-	var height float64
-	for {
-		fmt.Print("请输入身高(cm) : ")
-		height = stdinFloat()
-		if height > 0 {
-			break
-		}else {
-			fmt.Printf("请输入正确的身高\n")
-		}
-	}
-	return height
-}
-func getWeight() float64{
-	var weight float64
-	for {
-		fmt.Print("请输入体重(kg) : ")
-		weight = stdinFloat()
-		if weight > 0 {
-			break
-		}else {
-			fmt.Printf("请输入正确的体重\n")
-		}
-	}
-	return weight
-}
-func getExercise() int{
-	var exercise int
-	for {
-		fmt.Print("活动强度用 1 - 4 来表示 (轻体力劳动 -- 重体力劳动) : ")
-		exercise = stdinInt()
-		if exercise == 1 || exercise == 2 || exercise == 3 || exercise == 4{
-			break
-		}else {
-			fmt.Printf("请仅使用1 - 4 来表示\n")
-		}
-	}
-	return exercise
-}
-func getDiet() bool{
-	var diet bool
-	for {
-		fmt.Print("是否断用主食(t:是 f:否) : ")
-		diet = stdinBoolean()
-		if diet == true || diet == false {
-			break
-		}else {
-			fmt.Printf("请使用 't' 或 'f' 来回答\n")
-		}
-	}
-	return diet
 }
---
+++ Assignment/BMI/getInput/getInput.go
@@ -0,0 +1,92 @@
+package getInput
+
+import "fmt"
+
+func stdinInt() int {
+	var intReceiver int
+	_, err := fmt.Scanf("%d", &intReceiver)
+
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	return intReceiver
+}
+
+func stdinFloat() float64 {
+	var floatReceiver float64
+
+	_, err := fmt.Scanf("%f", &floatReceiver)
+
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	return floatReceiver
+}
+
+func stdinBoolean() bool {
+	var booleanReceiver bool
+
+	_, err := fmt.Scanf("%t", &booleanReceiver)
+
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	return booleanReceiver
+}
+
+
+func GetHeight() float64{
+	var height float64
+	for {
+		fmt.Print("请输入身高(cm) : ")
+		height = stdinFloat()
+		if height > 0 {
+			break
+		}else {
+			fmt.Printf("请输入正确的身高\n")
+		}
+	}
+	return height
+}
+func GetWeight() float64{
+	var weight float64
+	for {
+		fmt.Print("请输入体重(kg) : ")
+		weight = stdinFloat()
+		if weight > 0 {
+			break
+		}else {
+			fmt.Printf("请输入正确的体重\n")
+		}
+	}
+	return weight
+}
+func GetExercise() int{
+	var exercise int
+	for {
+		fmt.Print("活动强度用 1 - 4 来表示 (轻体力劳动 -- 重体力劳动) : ")
+		exercise = stdinInt()
+		if exercise == 1 || exercise == 2 || exercise == 3 || exercise == 4{
+			break
+		}else {
+			fmt.Printf("请仅使用1 - 4 来表示\n")
+		}
+	}
+	return exercise
+}
+func GetDiet() bool{
+	var diet bool
+	for {
+		fmt.Print("是否断用主食(t:是 f:否) : ")
+		diet = stdinBoolean()
+		if diet == true || diet == false {
+			break
+		}else {
+			fmt.Printf("请使用 't' 或 'f' 来回答\n")
+		}
+	}
+	return diet
+}
